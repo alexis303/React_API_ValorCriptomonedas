30a0e24642f378ce0a9914ba409f44b220fc7f89
alexis303 <gonzalosep98@gmail.com>
2020-11-10 09:35:57 -0300

    Muestra el valor de diferentes criptomonedas en diferentes monedas

---
+++ src/components/Cotizacion.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from '@emotion/styled'
+
+
+const ResultadoDiv = styled.div`
+    color: #aaa;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+
+    span {
+        font-weight: bold;
+    }
+`;
+
+const Precio = styled.p`
+    font-size: 30px;
+    span {
+        font-weight: bold;
+    }
+`;
+
+const Cotizacion = ({resultado}) => {
+   if(Object.keys(resultado).length ===0) return null;
+   console.log(resultado) 
+    return ( 
+        <ResultadoDiv>
+            <Precio>El precio es: <span>{resultado.PRICE}</span></Precio>
+            <Info>El precio más alto del dia: <span>{resultado.HIGHDAY}</span></Info>
+            <Info>El precio más bajo del dia: <span>{resultado.LOWDAY}</span></Info>
+            <Info>Variacion últimas 24 hrs: <span>{resultado.PRICE}</span></Info>
+            <Info>Última actualización: <span>{resultado.LASTUPDATE}</span></Info>
+        </ResultadoDiv>
+    );
+}
+ 
+export default Cotizacion;
